26ac2ae5281e772aad38e171090e232e6db23add
TakinosaJi <kostiantyn.chomakov@gmail.com>
2021-08-05 20:48:28 +0300

    Add Option class and extension methods

---
+++ CSharp.Option/CSharp.Option/Option.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp.Option
+{
+    public class Option<T>
+    {
+        public T Value { get; }
+        public bool HasObject { get; }
+
+        private Option()
+        {
+            HasObject = false;
+        }
+
+        private Option(T @object)
+        {
+            Value = @object;
+            HasObject = true;
+        }
+
+        public static Option<T> None { get; } = new();
+
+        public static Option<T> Some(T obj) => new(obj);
+    }
+}
---
+++ CSharp.Option/CSharp.Option/OptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.Option
+{
+    public static class OptionExtensions
+    {
+        public static Option<T> ToOption<T>(this T @object) =>
+            Option<T>.Some(@object);
+
+        public static T? ToNullable<T>(this Option<T> option)
+            where T : struct =>
+            option == Option<T>.None
+                ? null
+                : option.Value;
+    }
+}
