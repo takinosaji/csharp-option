98fd3487c7a19465edefe7eb8831ac9558dc500f
TakinosaJi <kostiantyn.chomakov@gmail.com>
2021-08-15 18:36:59 +0300

    Extend OptionExtensions

--- CSharp.Option/CSharp.Option/OptionExtensions.cs
+++ CSharp.Option/CSharp.Option/OptionExtensions.cs
@@ -2,13 +2,31 @@
 {
     public static class OptionExtensions
     {
-        public static Option<T> ToOption<T>(this T @object) =>
+        public static Option<T> ToSome<T>(this T @object) =>
             Option<T>.Some(@object);
 
+        public static Option<T> ToOption<T>(this T @object)
+            where T: class?
+        {
+            return @object is null
+                ? Option<T>.None
+                : Option<T>.Some(@object);
+        }
+
+        public static Option<T> DefaultToOption<T>(this T @object)
+            where T: struct
+        {
+            return @object.Equals(default)
+                ? Option<T>.None
+                : Option<T>.Some(@object);
+        }
+
         public static T? ToNullable<T>(this Option<T> option)
-            where T : struct =>
-            option == Option<T>.None
+            where T: struct
+        {
+            return option == Option<T>.None
                 ? null
                 : option.Value;
+        }
     }
 }
